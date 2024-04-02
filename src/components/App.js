756d52f9f901d10e29d3e1cf1ffb76a18f4a9d58
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-04-02 04:14:58 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,12 @@
-import React, { useState, useEffect } from "react";
-import "../styles/App.css";
-import Game from "./Game";
+import React from "react";
+import GolfGame from './GolfGame';
 
 const App = () => {
-//   const [renderBall, setRenderBall] = useState(false);
-//   const [ballPosition, setBallPosition] = useState({ left: "0px" });
-
-//   const buttonClickHandler = () => {
-//     setRenderBall(true);
-//   };
-
-//   const handleKeyDown = (event) => {
-//     if (event.key === "ArrowRight" && renderBall) {
-//       setBallPosition((prevPosition) => ({
-//         ...prevPosition,
-//         left: parseInt(prevPosition.left) + 5 + "px",
-//       }));
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("keydown", handleKeyDown);
-
-//     return () => {
-//       window.removeEventListener("keydown", handleKeyDown);
-//     };
-//   }, [renderBall]);
-
-//   const renderBallOrButton = () => {
-//     if (renderBall) {
-//       return <div className="ball" style={ballPosition}></div>;
-//     } else {
-//       return <button onClick={buttonClickHandler}>Start</button>;
-//     }
-//   };
-
-//   return <div className="playground">{renderBallOrButton()}</div>;
-return(
-    <Game/>
-)
+  return (
+    <div>
+      <GolfGame />
+    </div>
+  );
 };
 
 export default App;
